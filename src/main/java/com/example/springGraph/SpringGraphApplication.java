ec6cef8e7481736e3bcabf3c29ffa07a536f2196
Zho <chlwlgh1011@naver.com>
2024-03-07 21:22:08 +0900

    Initial commit

---
+++ src/main/java/com/example/springGraph/SpringGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.springGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphApplication.class, args);
+	}
+
+}
