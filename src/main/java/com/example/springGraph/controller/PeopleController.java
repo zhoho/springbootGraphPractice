8b59f65033f6182ef68d6303a4b3f23b16fa4076
Zho <chlwlgh1011@naver.com>
2024-03-17 17:39:46 +0900

    feat: controller 의존도 낮추기 ajax 활용

--- src/main/java/com/example/springGraph/controller/PeopleController.java
+++ src/main/java/com/example/springGraph/controller/PeopleController.java
@@ -1,17 +1,18 @@
 package com.example.springGraph.controller;
 
+import com.example.springGraph.entity.People;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import com.example.springGraph.service.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.springGraph.service.PeopleService;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/")
 public class PeopleController {
 
     private final PeopleService peopleService;
@@ -21,24 +22,34 @@ public class PeopleController {
         this.peopleService = peopleService;
     }
 
-    @GetMapping
-    public String getChartData(Model model) {
-        Map<Integer, Long> ageDistribution = peopleService.getAgeDistribution();
-        Long femaleCount = peopleService.getFemaleCount();
-        Map<Integer, Long> femalesByAgeGroup = peopleService.countFemalesByAgeGroup(); // 연령대별 여성 수 추가
-
-
-        model.addAttribute("ageDistribution", ageDistribution);
-        model.addAttribute("femaleCount", femaleCount);
-        model.addAttribute("femalesByAgeGroup", femalesByAgeGroup); // 모델에 연령대별 여성 수 추가
-
-        return "index";  // Thymeleaf 템플릿 반환
+    @GetMapping("/age-distribution")
+    @ResponseBody
+    public Map<Integer, Long> getAgeDistribution() {
+        return peopleService.getAgeDistribution();
     }
 
+    @GetMapping("/total-people")
+    @ResponseBody
+    public List<People> getTotalPeople() {
+        return peopleService.getAllPeople();
+    }
+
+    @GetMapping("/female/count")
+    @ResponseBody
+    public Long getFemaleCount() {
+        return peopleService.getFemaleCount();
+    }
+
+    @GetMapping("/female/age-distribution")
+    @ResponseBody
+    public Map<Integer, Long> getFemalesByAgeGroup() {
+        return peopleService.countFemalesByAgeGroup();
+    }
+
+    // 이미 고유한 경로가 있으므로 변경할 필요가 없습니다.
     @GetMapping("/male/age-distribution")
     @ResponseBody
     public Map<Integer, Long> getMaleAgeDistribution() {
         return peopleService.countMalesByAgeGroup();
     }
-
 }
