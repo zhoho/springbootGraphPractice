4a7d7b1b70f7781e2f5c2398b90d6d551339ee6d
Zho <chlwlgh1011@naver.com>
2024-04-13 14:46:04 +0900

    feat: 불필요 라인 삭제

--- src/main/resources/static/js/getweather.js
+++ src/main/resources/static/js/getweather.js
@@ -1,36 +1,85 @@
-$(document).ready(function() {
-    $('#getWeather').click(function() {
-        var lat = $('#latitude').val();
-        var lon = $('#longitude').val();
-        var apiKey = 'cb8af9f3317bb3f9118d23f3cae2dd15';
-        var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=kr`;
-
-        $.get(url, function(data) {
-            var location = data.name;
-            var temperature = Math.round(data.main.temp);
-            var weatherDescription = data.weather[0].description;
-            var iconCode = data.weather[0].icon;
-            var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
-            var windSpeed = data.wind.speed;
-            var windDirection = data.wind.deg;
-            var humidity = data.main.humidity;
-            var pressure = data.main.pressure;
-            var clouds = data.clouds.all;
-            var sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
-            var sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
-
-            console.log(data);
-
-            $('#location').html('위치: ' + location);
-            $('#weather-data').html('기온: ' + temperature + '°C, 상태: ' + weatherDescription +
-                '<img src="' + iconUrl + '">' +
-                ', 풍속: ' + windSpeed + ' m/s, 풍향: ' + windDirection + '°' +
-                ', 습도: ' + humidity + '%' +
-                ', 기압: ' + pressure + ' hPa' +
-                ', 구름량: ' + clouds + '%' +
-                ', 일출 시간: ' + sunrise + ', 일몰 시간: ' + sunset);
-        }).fail(function() {
-            $('#weather-data').html('날씨 정보를 불러올 수 없습니다.');
-        });
-    });
-});
+//
+// function getWeather(gridX, gridY) {
+//     console.log("Fetching weather for coordinates:", gridX, gridY); // Debugging log
+//     var xhr = new XMLHttpRequest();
+//     var url = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
+//     var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'hogAXADhx1TwUFYu9kBfwwvsv5m%2FwsLzvQT2tBT0BFSH3eLnXp88Dx%2FR6x2GCPtWKfaUQEPix2VcWYEp0Hz5Tw%3D%3D';
+//     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
+//     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('50');
+//     queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML');
+//     queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent('20240413');
+//     queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent('0500');
+//     queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent(gridX);
+//     queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent(gridY);
+//     xhr.open('GET', url + queryParams);
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState === 4 && this.status === 200) {
+//             // 날씨 데이터 처리
+//             displayWeatherData(this.responseXML);
+//             // console.log(this.responseText);
+//         }
+//     };
+//     xhr.send();
+// }
+//
+// function displayWeatherData(xml) {
+//     const items = xml.getElementsByTagName("item");
+//     let weatherData = {};
+//
+//     // 데이터 구조화
+//     for (let i = 0; i < items.length; i++) {
+//         const category = items[i].getElementsByTagName("category")[0].childNodes[0].nodeValue;
+//         const value = items[i].getElementsByTagName("fcstValue")[0].childNodes[0].nodeValue;
+//         const time = items[i].getElementsByTagName("fcstTime")[0].childNodes[0].nodeValue;
+//
+//         if (!weatherData[time]) {
+//             weatherData[time] = {};
+//         }
+//         weatherData[time][category] = value;
+//     }
+//
+//     // 테이블 생성
+//     let output = "<h3>Weather Forecast</h3>";
+//     output += "<table class='table table-striped table-hover'><thead class='table-dark'><tr><th>Time</th><th>Temperature (°C)</th><th>Wind Speed (m/s)</th><th>Humidity (%)</th><th>Precipitation</th><th>Sky Condition</th></tr></thead><tbody>";
+//
+//     for (const [time, data] of Object.entries(weatherData)) {
+//         output += `<tr>
+//                     <td>${time}</td>
+//                     <td>${data['TMP'] || '--'}</td>
+//                     <td>${data['WSD'] || '--'}</td>
+//                     <td>${data['REH'] || '--'}</td>
+//                     <td>${data['PCP'] || '--'}</td>
+//                     <td>${data['SKY'] || '--'}</td>
+//                   </tr>`;
+//     }
+//
+//     output += "</tbody></table>";
+//     document.getElementById("weather-data").innerHTML = output;
+// }
+//
+//
+// $(document).ready(function() {
+//     setupAutocomplete('#city', 'city', 'cityResults');
+//     setupAutocomplete('#district', 'district', 'districtResults');
+//     setupAutocomplete('#subdistrict', 'subdistrict', 'subdistrictResults');
+//
+//     function setupAutocomplete(selector, type, resultListId) {
+//         $(selector).on('input', function() {
+//             var input = $(this).val();
+//             if (input.length > 1) {
+//                 $.ajax({
+//                     url: '/searchAddress',
+//                     method: 'GET',
+//                     data: { type: type, searchTerm: input },
+//                     success: function(data) {
+//                         var autocompleteResults = $('#' + resultListId);
+//                         autocompleteResults.empty();
+//                         data.forEach(function(item) {
+//                             autocompleteResults.append($('<option>').val(item));
+//                         });
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// });
